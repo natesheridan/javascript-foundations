65c70625b5b6a4a973327ac923796faf64b8bd88
Nate Sheridan <nbs@dr.com>
2021-07-14 23:46:37 -0600

    Unicorn Test Completed

--- mythical-creatures/exercises/unicorn.js
+++ mythical-creatures/exercises/unicorn.js
@@ -1,8 +1,23 @@
 class Unicorn {
-  constructor(name, color) {
+  constructor(name, color = "white") {
     this.name = name;
     this.color = color;
   }
+
+  isWhite(){
+    if (this.color === "white"){
+      var isWhite = true
+    }
+    else {
+      var isWhite = false
+    }
+    return isWhite
+  }
+
+  says(phrase){
+    return `**;* ${phrase} *;**`
+  }
 }
+
 
 module.exports = Unicorn;
