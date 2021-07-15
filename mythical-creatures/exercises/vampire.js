b023703edcbf0066b9fb841d3b08a723329bcf80
Nate Sheridan <nbs@dr.com>
2021-07-15 00:39:17 -0600

    Complete vampire exercise

---
+++ mythical-creatures/exercises/vampire.js
@@ -0,0 +1,23 @@
+console.log("Vampire Test")
+
+class Vampire{
+    constructor(name , pet = "bat"){
+        this.name = name;
+        this.pet = pet;
+        this.thirsty = true;
+        this.ouncesDrank = 0;
+
+    }
+
+    drink(){
+        this.thirsty = false;
+        if (this.ouncesDrank >= 50){
+            return `I\'m too full to drink anymore!`
+        }
+        else{
+        this.ouncesDrank = this.ouncesDrank+10;
+        }
+    }
+}
+
+module.exports = Vampire;
