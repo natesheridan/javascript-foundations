38a05d243f8ffafba4115d49e607de0a3cca3ec6
Nate Sheridan <nbs@dr.com>
2021-07-15 00:11:45 -0600

    Complete dragon.js exercise

--- mythical-creatures/exercises/dragon.js
+++ mythical-creatures/exercises/dragon.js
@@ -1,1 +1,23 @@
+class Dragon{
+    constructor(name , rider) {
+        this.name = name;
+        this.rider = rider;
+        this.hunger = 0;
+        this.hungry = true;
+    }
 
+    greet(){
+        return `Hi, ${this.rider}!`
+    }
+
+    eat(){
+        this.hunger = this.hunger+1;
+        if (this.hunger > 2) {
+            this.hungry = false
+        }
+    }
+    
+};
+
+
+module.exports = Dragon;
